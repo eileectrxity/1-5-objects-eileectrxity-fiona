db5df3a85712cab93ad7d9fc074119da1bfb437a
electrxety <er191712@gmail.com>
2024-04-11 18:51:44 -0400

    solved question 3

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -33,21 +33,50 @@ const coolGreeting = (person) => {
 // console.log(coolGreeting(coolPerson)) //"What is UP SARA? How you been doin'?"
 // console.log(coolGreeting(notCoolPerson)) //'Greetings Bob, how have you been lately?'
 
-const haveBirthday = (person) => { // We are creating the function 
-  const { name, age } = person //definding the parameter person as an object with key variables called name and age
-  person.age = age + 1 //increasing the age key value by 1
+/*Question 2: a function that takes in an object, person. it should increase their age property by one; the function returns nothing
+gint: be careful with destructuring when modifying object properties. */
+const haveBirthday = (person) => {
+  const { name, age } = person //defining the parameter person as an object with key variables called name and age
+    person.age = age + 1 //increasing the age key value by 1
 };
 
-/*
-  const age1 = 30;
-  const age2 = 0;
-  const age3 = 100;
-  const person1 = { name: 'Sara', age: age1 };
-  const person2 = { name: 'Bob', age: age2 };
-  const person3 = { name: 'Jo', age: age3 };
-*/
-const becomeSecretAgent = () => {
+// //printing results to the console below
+//   const age1 = 30;
+//   const age2 = 0;
+//   const age3 = 100;
+//   const person1 = { name: 'Sara', age: age1 };
+//   const person2 = { name: 'Bob', age: age2 };
+//   const person3 = { name: 'Jo', age: age3 };
+
+// haveBirthday(person1);
+// console.log(person1.age); //age1 + 1
+// haveBirthday(person2);
+// console.log(person2.age) //age2 + 1
+// haveBirthday(person3);
+// console.log(person3.age) //age3 + 1
+
+//Question 3 = a function that takes 2 arguments: an object person and a string spyHandle. function should delete the name property from person and add the spyHandle value as a property with the same name
+const becomeSecretAgent = (person, spyHandle) => {
+  const { name, age } = person
+  person.spyHandle = spyHandle
+  delete person.name
 };
+
+// //printing results to the console below
+//   const person1 = {
+//     name: 'Sara',
+//     age: 30,
+//   };
+
+//   const person2 = {
+//     name: 'Sara',
+//     age: 30,
+//   };
+
+// becomeSecretAgent(person1, '007');
+// console.log(person1); //{ spyHandle: '007', age: 30 }
+// becomeSecretAgent(person2, '008');
+// console.log(person2); //{ spyHandle: '008', age: 30 }
 
 const carMaker = () => {
 };
